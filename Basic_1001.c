de6bba5ebef4595660fce473c2e27a6c17fafb0d
Juneuly <perhaps1915@gmail.com>
2017-07-26 19:16:31 +0800

    Update Basic_1001.c

--- Basic_1001.c
+++ Basic_1001.c
@@ -1,4 +1,23 @@
 //
 // Created by wsx on 2017/7/26.
 //
+#include <stdio.h>
 
+void Basic_1001() {
+    int n;
+    int flag;
+    flag=0;
+    scanf("%d",&n);
+    while(n!=1){
+        if(n%2==0){
+            n=n/2;
+            flag++;
+        }
+        else{
+            n=(3*n+1)/2;
+            flag++;
+        }
+    }
+    printf("%d",flag);
+
+}
